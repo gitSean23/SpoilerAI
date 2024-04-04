46b64fc02f1b6bfe0a860ac966f2f4952d1329f6
gitSean23 <the.feldman23@gmail.com>
2024-04-04 01:42:08 -0400

    Add initial spoiler detection to content script

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,2 +1,9 @@
 // src/contentScript.ts
 console.log("Content script loaded");
+import { spoilerRegex } from "./spoilerWords";
+
+document.querySelectorAll('h1, h3, p, .article-body, .comment').forEach(element => {
+    if (spoilerRegex.spoiler.test(element.textContent as string) || spoilerRegex.ending.test(element.textContent as string) || spoilerRegex.explained.test(element.textContent as string)) {
+        element.textContent = "SPOILER DETECTED!";
+    }
+})
---
+++ src/spoilerWords.ts
@@ -0,0 +1,5 @@
+export const spoilerRegex = {
+    spoiler: /(?:spoiler|spoilers)\b/i,
+    explained: /explain(ed|s)?/i,
+    ending: /(?:ending|endings)\b/i
+}
